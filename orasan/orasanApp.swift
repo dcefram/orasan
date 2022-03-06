f9a1e6a778c5b901ce53e7774de8d6d0aa57bb59
Daniel Cefram S. Ramirez <dc@rmrz.ph>
2022-03-06 09:51:32 +0800

    Initial Commit

---
+++ orasan/orasanApp.swift
@@ -0,0 +1,17 @@
+//
+//  orasanApp.swift
+//  orasan
+//
+//  Created by dc on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct orasanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
